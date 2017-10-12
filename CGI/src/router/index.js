7b3a65a383e8be7f7c0702405bae7188d10948b3
Elias Basler <e.e.basler@gmail.com>
2017-10-12 10:54:51 -0400

    Add routes

--- CGI/src/router/index.js
+++ CGI/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Register from '@components/Register'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
