8879c73d851f22ae6d80024bc660487043e7a53f
Elias Basler <e.e.basler@gmail.com>
2017-10-18 12:08:01 -0400

    Add register

--- CGI/src/components/Register.vue
+++ CGI/src/components/Register.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="register">
-    <h1>{{ msg }}</h1>
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"/>
+    <br>
+    <input 
+      type="pasword"
+      name="password"
+      v-model="password"
+      placeholder="password"/>
+    <br>
+    <button @click="register">
+      Register
+    </button>
   </div>
 </template>
 
 <script>
+import AuthService from '@/services/AuthenticationService.js';
 export default {
   name: 'register',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async register() {
+      console.log('click');
+      const response = await AuthService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
